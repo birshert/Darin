e0fc6f0f5d06b0d6db7f265a55edae0f028330cc
Birshert Alexey <34584079+birshert@users.noreply.github.com>
2019-03-12 15:58:28 +0300

    Delete Functions.py

--- NET/Functions.py
+++
@@ -1,7 +0,0 @@
-def possible_moves(field):
-    moves = []
-    for i in range(field.get_size()):
-        for j in range(field.get_size()):
-            if field.get_node(i, j).is_empty():
-                moves.append([i, j])
-    return moves
